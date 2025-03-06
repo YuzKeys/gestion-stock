f4189c3133c67d817d5148e2944d4ec9a27a5c14
Youssef Larbi <larbiyoussef93@gmail.com>
2025-03-06 22:55:03 +0100

    Mise a jour du productController

--- controlleurs/product.controller.js
+++ controlleurs/product.controller.js
@@ -1,17 +1,28 @@
+import { ProductDetailDTO, ProductDTO } from '../DTO/product.dto.js'
 import Product from "../models/Product.js";
-
 
 const productController = {
 
+    // Afficher la liste des produits
     getAllProducts: async (req, res) => {
-        const products = await Product.find(); //Trouver Tous les produits
-        res.status(200).json(products) // Retourner tous les produits
-    },
+        // Gestion de la pagination //! /api/product?offset=2&limit=2
+        const { offset, limit } = req.pagination;
 
-    getProductById: (req, res) => {
+        // Récuperation des produits en DTO avec Gestion d'erreur en cas d'echec
+        try {
+            const productList = await Product.find(); //Trouver Tous les produits
+            const products = productList.map(p => new ProductDTO(p)); //Mapping et renvoie des produits en DTO
+            res.status(200).json(products) // Retourner tous les produits
+
+        } catch (err) {
+            res.status(500).json({ message: "Erreur lors de la recuperation des Produits", error: err });
+        }
+    },
+    getProductDetailById: async (req, res) => {
         res.sendStatus(501);
     },
 
+    // Ajouter des produits
     addNewProduct: async (req, res) => {
         try {
             // Créer une nouvelle instance du produit à partir des données envoyées par le client
