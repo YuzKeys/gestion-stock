db881e233da71e13f48499df9cedb7c93876ec9c
Youssef Larbi <larbiyoussef93@gmail.com>
2025-03-07 20:12:12 +0100

    mise a jour du routeur principale

--- routeurs/index.js
+++ routeurs/index.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import productRouter from './productRouter.js';
+import productRouter from './product.router.js';
+import authRouter from './auth.router.js';
 
 
 const mainRouter = Router();
 
 mainRouter.use('/product', productRouter)
+mainRouter.use("/auth", authRouter);
 
 export default mainRouter;
